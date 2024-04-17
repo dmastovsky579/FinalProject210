8e271bc461779396235df83d007a91fe815cf13e
petersn86 <133061453+petersn86@users.noreply.github.com>
2024-04-16 21:27:39 -0400

    added templates to each page, as well as a home + signout button system

--- app/src/main/java/com/zybooks/finalproject210/FormPage.java
+++ app/src/main/java/com/zybooks/finalproject210/FormPage.java
@@ -1,5 +1,6 @@
 package com.zybooks.finalproject210;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -81,6 +82,22 @@ public class FormPage extends AppCompatActivity {
         }
         if (cursor != null) {
             cursor.close();
+        }
+    }
+
+    public void onImageViewClick(View view) {
+        // Determine which ImageView was clicked
+        int viewId = view.getId();
+
+        // Handle the click based on the ImageView's ID
+        if (viewId == R.id.signOutImageView) {
+            // Redirect to the log in activity
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        } else if (viewId == R.id.homeImageView) {
+            // Redirect to the home activity
+            Intent intent = new Intent(this, HomeActivity.class);
+            startActivity(intent);
         }
     }
 
--- app/src/main/java/com/zybooks/finalproject210/Page2Activity.java
+++ app/src/main/java/com/zybooks/finalproject210/Page2Activity.java
@@ -1,6 +1,9 @@
 package com.zybooks.finalproject210;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Page2Activity extends AppCompatActivity {
@@ -8,5 +11,21 @@ public class Page2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page2);
+    }
+
+    public void onImageViewClick(View view) {
+        // Determine which ImageView was clicked
+        int viewId = view.getId();
+
+        // Handle the click based on the ImageView's ID
+        if (viewId == R.id.signOutImageView) {
+            // Redirect to the log in activity
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        } else if (viewId == R.id.homeImageView) {
+            // Redirect to the home activity
+            Intent intent = new Intent(this, HomeActivity.class);
+            startActivity(intent);
+        }
     }
 }
